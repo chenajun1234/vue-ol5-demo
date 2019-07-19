7ab5341c959d2969f6c0e9fbe86e1a487a8a363f
chenajun1234 <499187079@qq.com>
2019-07-19 18:07:42 +0800

    ol 版本更新

--- src/components/ol/WebGLDemo.vue
+++ src/components/ol/WebGLDemo.vue
@@ -1,5 +1,8 @@
 <template>
-    <div id="map" class="map"></div>
+   <div>     
+      <div id="map" class="map"></div>
+      <div id="year"></div>
+   </div>
 </template>
 <script>
 import 'ol/ol.css';
@@ -13,6 +16,8 @@ import Point from 'ol/geom/Point';
 import Renderer from 'ol/renderer/webgl/PointsLayer';
 import { clamp } from 'ol/math';
 const color = [1, 0, 0, 0.5];
+//我们将使用WebGL PointsLayer渲染器创建自定义图层。要创建自定义图层，
+//我们需要覆盖createRenderer返回渲染器实例的函数。我们使用colorCallback和构造此渲染器sizeCallback来确定渲染点的颜色和大小。
 class CustomLayer extends VectorLayer {
     createRenderer() {
         return new Renderer(this, {
@@ -21,19 +26,37 @@ class CustomLayer extends VectorLayer {
             },
             sizeCallback: function(feature) {
                 return 18 * clamp(feature.get('mass') / 200000, 0, 1) + 8;
-            }
+            },
+            //要将我们的点渲染为圆圈(本来是方块)，我们需要创建自定义片段着色器。我们将着色器作为fragmentShader渲染器构造函数的第二个参数的字符串属性传递给WebGL点图层渲染器。片段着色器是用OpenGL着色语言（GLSL）编写的。如果您有兴趣了解有关着色器和WebGL的更多信息，请查看着色器。
+            fragmentShader: `
+              precision mediump float;
+              varying vec2 v_texCoord;
+              varying vec4 v_color;
+              void main(void) {
+                vec2 texCoord = v_texCoord * 2.0 - vec2(1.0, 1.0);
+                float sqRadius = texCoord.x * texCoord.x + texCoord.y * texCoord.y;
+                float value = 2.0 * (1.0 - sqRadius);
+                float alpha = smoothstep(0.0, 1.0, value);
+                gl_FragColor = v_color;
+                gl_FragColor.a *= alpha;
+              }`
         });
     }
 }
+const minYear = 1850;
+const maxYear = 2015;
+const span = maxYear - minYear;
+const rate = 10; // years per second
 
+const start = Date.now();
+let currentYear = minYear;
 export default {
     name: 'WebGLDemo',
     data() {
         return {
             countries: [], //geoJson对象  
             mapObj: null,
-            mapSource: null
-
+            mapSource: null           
         }
     },
     created() {
@@ -43,12 +66,11 @@ export default {
 
         this.$ajax.get("http://localhost:9999/src/static/data/meteorites.csv")
             .then(res => {
-                debugger;
                 const csv = res.data;
                 const features = [];
                 let prevIndex = csv.indexOf('\n') + 1; // scan past the header line
                 let curIndex;
-                while ((curIndex = csv.indexOf('\n', prevIndex)) != -1) {
+                while ((curIndex = csv.indexOf('\n', prevIndex)) != -1 && prevIndex <= 665336) {
                     const line = csv.substr(prevIndex, curIndex - prevIndex).split(',');
                     prevIndex = curIndex + 1;
                     const coords = fromLonLat([parseFloat(line[4]), parseFloat(line[3])]);
@@ -84,7 +106,30 @@ export default {
                 center: [31.8797296333, 106.2118975625],
                 zoom: 3
             })
-        })
+        });
+      this.render();
+    },
+    methods:{
+      render() {
+        let yearElement = document.getElementById('year');
+        let elapsed = rate * (Date.now() - start) / 1000;
+        currentYear = minYear + (elapsed % span);
+        yearElement.innerText = currentYear.toFixed(0);
+
+        this.mapObj.render();
+        requestAnimationFrame(this.render);
+      }
     }
 }
 </script>
+<style type="text/css" media="screen">
+  #year {
+    position: absolute;
+    bottom: 1em;
+    left: 1em;
+    color: white;
+    -webkit-text-stroke: 1px black;
+    font-size: 2em;
+    font-weight: bold;
+  }
+</style>
